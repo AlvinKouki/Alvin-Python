49a0459d104708a04766c93ed605676bb284b60a
AlvinKouki <05153@stu.kcis.ntpc.edu.tw>
2022-03-13 10:15:53 +0800

    create class file

--- 1100310/20220310.py
+++ 1100310/20220310.py
@@ -17,26 +17,25 @@ bg = pygame.Surface(screen.get_size())
 #                 math.radians(0), 2)
 # pygame.draw.line(bg, (0, 255, 255), (300, 200), (100, 100), 1)
 
-
-
 # pygame.draw.ellipse(bg,(0,0,0),[250,250,340,340],0)
 # pygame.draw.circle(bg, (255, 0, 0), (500, 400), 30, 0)
 # pygame.draw.circle(bg, (255, 0, 0), (350, 400), 30, 0)
 # pygame.draw.arc(bg, (80, 80, 80), [350, 400, 150, 110], math.radians(180),math.radians(0), 2)
 
-picture=False
+picture = False
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if event.type==pygame.MOUSEBUTTONDOWN:
-            picture = not(picture)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            picture = not (picture)
 
     if picture:
-        pygame.draw.ellipse(bg,(0,0,0),[250,250,340,340],0)
+        pygame.draw.ellipse(bg, (0, 0, 0), [250, 250, 340, 340], 0)
         pygame.draw.circle(bg, (255, 0, 0), (500, 400), 30, 0)
         pygame.draw.circle(bg, (255, 0, 0), (350, 400), 30, 0)
-        pygame.draw.arc(bg, (80, 80, 80), [350, 400, 150, 110], math.radians(180),math.radians(0), 2)
+        pygame.draw.arc(bg, (80, 80, 80), [350, 400, 150, 110],
+                        math.radians(180), math.radians(0), 2)
     else:
         bg.fill((83, 197, 192))
 
