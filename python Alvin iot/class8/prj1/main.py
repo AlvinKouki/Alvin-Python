1af6930028fb98bab694023cd9090244adccff97
AlvinKouki <05153@stu.kcis.ntpc.edu.tw>
2022-11-13 11:08:09 +0800

    create

---
+++ python Alvin iot/class8/prj1/main.py
@@ -0,0 +1,38 @@
+import time
+from mcu_def import gpio, mcu_fun
+
+wlan = mcu_fun()
+wlan.connect_ap("SingularClass0", "Singular#1234")
+RED, GREEN, BLUE = wlan.led_initial(r_pin=gpio.D5,
+                                    b_pin=gpio.D6,
+                                    g_pin=gpio.D6,
+                                    mode='pwm')
+
+lcd = wlan.lcd_initial(sda_pin=4, scl_pin=5)
+wlan.mqtt_subscribe(mq_id='Alvin')
+
+
+def on_message(topic, msg):
+    msg = msg.decode("utf-8")
+    topic = topic.decode("utf-8")
+    print(f"my subscribe topic:{topic}, msg:{msg}")
+    lcd.clear()
+    lcd.putstr(f"topic:{topic}")
+    lcd.move_to(0, 1)
+    lcd.putstr(f"msg:{msg}")
+    if msg == "on":
+        RED.duty(1023)
+        GREEN.duty(1023)
+        BLUE.duty(1023)
+    elif msg == "off":
+        RED.duty(0)
+        GREEN.duty(0)
+        BLUE.duty(0)
+
+
+wlan.mqtt_get_msg(on_message=on_message, topic='Alvin')
+
+while 1:
+    wlan.mqClient0.check_msg()
+    wlan.mqClient0.ping()
+    time.sleep(0.3)
--- python Alvin iot/common/mcu_def.py
+++ python Alvin iot/common/mcu_def.py
@@ -1,4 +1,7 @@
 import network
+from machine import Pin, I2C, PWM
+from esp8266_i2c_lcd import I2cLcd
+from umqtt.simple import MQTTClient
 
 
 class gpio:
@@ -19,6 +22,7 @@ class mcu_fun:
 
     def _init_(self):
         self.ip = None
+        self.mqClient0 = None
 
     def connect_ap(self, ssid, pwd):
         wlan = network.WLAN(network.STA_IF)
@@ -31,3 +35,46 @@ class mcu_fun:
             pass
         print('network config:', wlan.ifconfig())
         self.ip = wlan.ifconfig()[0]
+
+    def led_initial(self, r_pin, g_pin, b_pin, mode: str = 'digital'):
+        if mode.lower() == 'digital':
+            RED = Pin(r_pin, Pin.OUT)
+            BLUE = Pin(b_pin, Pin.OUT)
+            GREEN = Pin(g_pin, Pin.OUT)
+            RED.value(0)
+            BLUE.value(0)
+            GREEN.value(0)
+        else:
+            f = 1000
+            d = 0
+            RED = PWM(Pin(gpio.D5), freq=f, duty=d)
+            GREEN = PWM(Pin(gpio.D6), freq=f, duty=d)
+            BLUE = PWM(Pin(gpio.D7), freq=f, duty=d)
+        return (RED, GREEN, BLUE)
+
+    def lcd_initial(self, scl_pin, sda_pin):
+        i2c = I2C(scl=Pin(scl_pin), sda=Pin(sda_pin), freq=400000)
+        lcd = I2cLcd(i2c, 0x27, 2, 16)
+        return lcd
+
+    def mqtt_subscribe(self, mq_id: str):
+        mq_server = "singularmakers.asuscomm.com"
+        mq_user = "singular"
+        mq_pass = "1234"
+        self.mqClient0 = MQTTClient(mq_id,
+                                    mq_server,
+                                    user=mq_user,
+                                    password=mq_pass,
+                                    keepalive=30)
+
+        try:
+            self.mqClient0.connect()
+        except Exception as e:
+            print(e)
+            exit()
+        finally:
+            print("connected MQTT server")
+
+    def mqtt_get_msg(self, on_message, topic: str):
+        self.mqClient0.set_callback(on_message)
+        self.mqClient0.subscribe(topic)
